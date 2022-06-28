c1c9c0dfa19db7e9cd93a97b77ae487fdc67ea3c
ricardamarai <ricarda.maria@gmx.de>
2022-06-28 11:17:11 +0200

    Gallery masonry grid

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-
-
 var artistsBackup = new Array("Angela", "AnnaLena", "Eli", "Elli", "JuliaC", "JuliaP", "Juliana", "LauraH", "LauraM", "Leslie", "Magdalena", "Nassim", "Nataliia", "Raffaela", "Ricarda", "Samuel", "Sandra", "Thao", "Tobias", "Yunuo");
 var artists = artistsBackup.slice();
 
 function createThumbnails() {
-  let artworkCount = 0;
-  while (artists.length > 0) {
-    if (artworkCount % 5 == 0) {
-      document.getElementById("gallery").append(document.createElement('br'));
+    let artworkCount = 0;
+    while (artists.length > 0) {
+        // if (artworkCount % 5 == 0) {
+        //   document.getElementById("gallery").append(document.createElement('br'));
+        // }
+        artworkCount++;
+        let newThumbnail = document.createElement('img');
+        let currentArtist = artists[Math.floor(Math.random() * artists.length)];
+        newThumbnail.src = "ArtworkPreviews/Artist=PP " + currentArtist + ".jpg";
+
+
+        newThumbnail.setAttribute('class', 'galleryImage');
+        // newThumbnail.width = "";
+        // newThumbnail.height = "auto";
+        document.getElementById("gallery").append(newThumbnail);
+        //newThumbnail.addEventListener("click", presentArtwork(currentArtist));
+        artists = artists.filter((artist) => { return artist != currentArtist });
     }
-    artworkCount++;
-    let newThumbnail = document.createElement('img');
-    let currentArtist = artists[Math.floor(Math.random() * artists.length)];
-    newThumbnail.src = "ArtworkPreviews/Artist=PP " + currentArtist + ".jpg";
-    newThumbnail.width = "128";
-    newThumbnail.height = "128";
-    document.getElementById("gallery").append(newThumbnail);
-    //newThumbnail.addEventListener("click", presentArtwork(currentArtist));
-    artists = artists.filter((artist) => { return artist != currentArtist });
-  }
 }
 
 
