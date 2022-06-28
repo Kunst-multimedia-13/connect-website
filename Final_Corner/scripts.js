93941f109e9484c0ee75f771dc5540076a8777fc
Nassim-Ch <N.Chamseddine@campus.lmu.de>
2022-06-28 17:37:16 +0200

    Tried out hover w/ names

--- Final_Corner/scripts.js
+++ Final_Corner/scripts.js
@@ -1,5 +1,7 @@
 var artistsBackup = new Array("Angela", "AnnaLena", "Eli", "Elli", "JuliaC", "JuliaP", "Juliana", "LauraH", "LauraM", "Leslie", "Magdalena", "Nassim", "Nataliia", "Raffaela", "Ricarda", "Samuel", "Sandra", "Thao", "Theresa", "Tobias", "Yunuo");
+var artistFullName_b = new Array("Angela Denninger", "Anna Lena Welter", "Elisabeth Frank", "Elisabeth Elmauer", "Julia Cox", "Julia Prottengeier", "Juliana Gutiérrez Wiest", "Laura Hollmann", "Laura Mayr", "Leslie Scholl", "Magdalena Ammer", "Nassim Chamseddine", "Nataliia Daliba", "Raffaela Kammer", "Ricarda Jocher", "Samuel Brookman-Amissah", "Sandra Kienle", "Diep-Thao Pham", "Theresa Zimmermann", "Tobias Biber", "Yunuo Zhang");
 var artists = artistsBackup.slice();
+var artistsFullName = artistFullName_b.slice();
 
 function createThumbnails() {
     let artworkCount = 0;
@@ -8,11 +10,12 @@ function createThumbnails() {
         //     document.getElementById("gallery").append(document.createElement('br'));
         // }
         artworkCount++;
-        let newThumbnail = document.createElement('input');
-        let currentArtist = artists[Math.floor(Math.random() * artists.length)];
+        let newThumbnail = document.createElement('button');
+        let currentNum = Math.floor(Math.random() * artists.length);
+        let currentArtist = artists[currentNum];
         newThumbnail.type = "submit";
         newThumbnail.className = "galleryImage";
-        newThumbnail.value = currentArtist;
+        newThumbnail.value = currentArtist + currentNum;
         newThumbnail.name = "artist";
 
         //newThumbnail.setAttribute('class', 'galleryImage');
@@ -22,6 +25,7 @@ function createThumbnails() {
         // document.getImage
 
         newThumbnail.style = "background-image: url('ArtworkPreviews/Artist=PP " + currentArtist + ".jpg');";
+        //newThumbnail.innerHTML = artistFullName_b[currentNum];
         document.getElementById("gallery").append(newThumbnail);
         artists = artists.filter((artist) => { return artist != currentArtist });
     }
